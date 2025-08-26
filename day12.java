6d76ffbb120fe46d5e0885145bb69723f2ec8ab7
Rohit Dixit <150375525+RD-git-hub@users.noreply.github.com>
2025-08-26 23:08:11 +0530

    Create day12.java

---
+++ day12.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class ValidParentheses {
+    public static boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else {
+                if (st.isEmpty()) return false;
+                char top = st.pop();
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String s = "[{()}]";
+        System.out.println(isValid(s)); // true
+    }
+}
